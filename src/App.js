9c8e51adb8774eeb9b7a201620db46f387f73c47
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-18 19:37:21 -0300

    moving actions and reducers to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import React, { createContext } from 'react';
+
+const MyContext = createContext(defaultValue);
 
 function App() {
   return (
