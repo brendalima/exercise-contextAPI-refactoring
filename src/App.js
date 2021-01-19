f48b78e70fa3e8b296af14d19e50f8b6220402d0
brendalima <brenda.c.lima@poli.ufrj.br>
2021-01-19 19:07:41 -0300

    refactoring to use context instead of redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
-  );
+export const MyContext = createContext();
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.moveCar = this.moveCar.bind(this);
+    this.changeSignal = this.changeSignal.bind(this);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+      signal: { color: 'red' },
+    };
+  }
+
+  moveCar(car, side) {
+    this.setState({cars:{[car]: side}});
+  }
+
+  changeSignal(color) {
+    this.setState({signal:{color: color}});
+  }
+  
+  render() {
+    const contextValue = {
+      cars: this.state.cars,
+      moveCar: (car , side ) => this.moveCar(car,side),
+      changeSignal: (color) => this.changeSignal(color),
+      signalColor: this.state.signal.color,
+    }
+    return (
+      <MyContext.Provider value={contextValue}>
+        <div className="container">
+          <Cars />
+          <TrafficSignal />
+        </div>
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
